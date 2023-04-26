b2c765b9e5f46d33b1bb8155293dabdf59a08543
Sameer <sameer4363@gmail.com>
2023-04-25 22:39:53 -0400

    Add working shuffle function to card grid

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -3,9 +3,10 @@ import Card from './Card';
 
 const CardGrid = (props) => {
   const { grid, handleClick } = props;
+  const shuffledGrid = shuffle([...grid]);
   return (
     <div className="card-grid">
-      {grid?.map((cardInfo) => (
+      {shuffledGrid?.map((cardInfo) => (
         <Card
           key={cardInfo.id}
           id={cardInfo.id}
@@ -15,6 +16,15 @@ const CardGrid = (props) => {
       ))}
     </div>
   );
+
+  function shuffle(array) {
+    // Fisher-Yates (aka Knuth) shuffle algorithm
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 };
 
 export default CardGrid;
